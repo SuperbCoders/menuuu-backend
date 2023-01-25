079a06f3198fe1bb0a897c17dbf38dbc2c2fe7c5
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2023-01-25 11:27:40 +0300

    Добавить тесты для API просмотра блюд

--- menu_backend/menus/tests/test_api/test_v1/test_menucourse.py
+++ menu_backend/menus/tests/test_api/test_v1/test_menucourse.py
@@ -48,6 +48,33 @@ class MenuCourseListTest(BaseTestCase):
         self.assertEqual(info['count'], 4)
         self.assertEqual(len(info['results']), 4)
 
+    def test_premium_worker(self):
+        """Работник ресторана не видит неопубликованных блюд другого ресторана"""
+        with self.logged_in('premium_worker'):
+            ans = self.client.get(self.__get_url())
+        self.assertEqual(ans.status_code, 200)
+        info = ans.json()
+        self.assertEqual(info['count'], 3)
+        self.assertEqual(len(info['results']), 3)
+
+    def test_premium_owner(self):
+        """Хозяин ресторана не видит неопубликованных блюд другого ресторана"""
+        with self.logged_in('premium_owner'):
+            ans = self.client.get(self.__get_url())
+        self.assertEqual(ans.status_code, 200)
+        info = ans.json()
+        self.assertEqual(info['count'], 3)
+        self.assertEqual(len(info['results']), 3)
+
+    def test_admin(self):
+        """Администратор видит все бдюда включая неопубликованные"""
+        with self.logged_in('admin'):
+            ans = self.client.get(self.__get_url())
+        self.assertEqual(ans.status_code, 200)
+        info = ans.json()
+        self.assertEqual(info['count'], 4)
+        self.assertEqual(len(info['results']), 4)
+
 
 class PublishedMenuCourseRetrieveTest(BaseTestCase):
     """
@@ -60,5 +87,12 @@ class PublishedMenuCourseRetrieveTest(BaseTestCase):
     def test_unauthorized(self):
         """Неавторизованный пользователь может видеть опубликованное блюдо"""
         ans = self.client.get(self.__get_url())
+        self.assertEqual(ans.status_code, 200)
+        self.verify_chocolate_sandwich(ans.json())
+
+    def test_some_user(self):
+        """Зарегистрированный пользователь может видеть опубликованное блюдо"""
+        with self.logged_in('some_user'):
+            ans = self.client.get(self.__get_url())
         self.assertEqual(ans.status_code, 200)
         self.verify_chocolate_sandwich(ans.json())
