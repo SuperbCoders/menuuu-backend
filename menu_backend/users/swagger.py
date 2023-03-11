0870cf0b4ad121ec0eb452f1954b3101175586ec
Alex Itkes <38556752+alexitkes@users.noreply.github.com>
2023-03-11 16:19:42 +0300

    Сваггер-описание для списка ресторанов пользователя, расписано вручную с правильным описанием переводов

--- menu_backend/users/swagger.py
+++ menu_backend/users/swagger.py
@@ -138,6 +138,7 @@ swagger_user_restaurants = swagger_auto_schema(
                         description=_("The number of the current user's restaurants")
                     ),
                     'results': openapi.Schema(
+                        title=_("Restaurant list"),
                         type=openapi.TYPE_ARRAY,
                         description=_("The list of the current user's restaurants"),
                         items=openapi.Items(
@@ -148,11 +149,13 @@ swagger_user_restaurants = swagger_auto_schema(
                                     type=openapi.TYPE_INTEGER
                                 ),
                                 'translations': openapi.Schema(
-                                    _("The name and the description of the restaurant, in different languages"),
+                                    title=_("Name and description"),
+                                    description=_("The name and the description of the restaurant, in different languages"),
                                     type=openapi.TYPE_OBJECT,
                                     properties={
                                         'en': openapi.Schema(
-                                            _("The name and the description of the restaurant, in English"),
+                                            title=_("In English"),
+                                            description=_("The name and the description of the restaurant, in English"),
                                             type=openapi.TYPE_OBJECT,
                                             properties={
                                                 'name': openapi.Schema(
@@ -169,22 +172,26 @@ swagger_user_restaurants = swagger_auto_schema(
                                 ),
                                 'slug': openapi.Schema(
                                     description=_("The short URL-friendly name of the restaurant (A-Z, 0-9 and - symbols only)"),
-                                    type=openapi.TYPE_STRING
+                                    type=openapi.TYPE_STRING,
+                                    format="slug"
                                 ),
                                 'logo': openapi.Schema(
                                     description=_("The restaurant's logo image URL"),
-                                    type=openapi.TYPE_STRING
+                                    type=openapi.TYPE_STRING,
+                                    format="uri"
                                 ),
                                 'picture': openapi.Schema(
                                     description=_("The restaurant's picture URL"),
-                                    type=openapi.TYPE_STRING
+                                    type=openapi.TYPE_STRING,
+                                    format="uri"
                                 ),
                                 'category': openapi.Schema(
                                     description=_("Restaurant's category ID"),
                                     type=openapi.TYPE_INTEGER
                                 ),
                                 'category_data': openapi.Schema(
-                                    _("Restaurant's category data"),
+                                    title=_("RestaurantCategory"),
+                                    description=_("Restaurant's category data"),
                                     type=openapi.TYPE_OBJECT,
                                     properties={
                                         'id': openapi.Schema(
@@ -192,11 +199,13 @@ swagger_user_restaurants = swagger_auto_schema(
                                             type=openapi.TYPE_INTEGER
                                         ),
                                         'translations': openapi.Schema(
-                                            _("The name of the restaurant category, in different languages"),
+                                            title=_("Category name"),
+                                            desctiption=_("The name of the restaurant category, in different languages"),
                                             type=openapi.TYPE_OBJECT,
                                             properties={
                                                 'en': openapi.Schema(
-                                                    _("The name of the restaurant category, in English"),
+                                                    title=_("In English"),
+                                                    description=_("The name of the restaurant category, in English"),
                                                     type=openapi.TYPE_OBJECT,
                                                     properties={
                                                         'name': openapi.Schema(
@@ -213,7 +222,63 @@ swagger_user_restaurants = swagger_auto_schema(
                                     description=_("Number of stars, greater means better and more expensive"),
                                     type=openapi.TYPE_INTEGER
                                 ),
-                            }
+                                'country': openapi.Schema(
+                                    description=_("Restaurant location country"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'city': openapi.Schema(
+                                    description=_("Restaurant location city"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'street': openapi.Schema(
+                                    description=_("Restaurant location street"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'building': openapi.Schema(
+                                    description=_("Restaurant's building number"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'address_details': openapi.Schema(
+                                    description=_("Restaurant address extra information (entrance number, floor, etc.)"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'zip_code': openapi.Schema(
+                                    description=_("Restaurant's address ZIP code"),
+                                    type=openapi.TYPE_STRING
+                                ),
+                                'longitude': openapi.Schema(
+                                    description=_("Restaurant's location longitude"),
+                                    type=openapi.TYPE_STRING,
+                                    format="decimal"
+                                ),
+                                'latitude': openapi.Schema(
+                                    description=_("Restaurant's location latitude"),
+                                    type=openapi.TYPE_STRING,
+                                    format="decimal"
+                                ),
+                                'phone': openapi.Schema(
+                                    description=_("Restaurant's phone number"),
+                                    type=openapi.TYPE_STRING,
+                                    format="phone"
+                                ),
+                                'site': openapi.Schema(
+                                    description=_("Restaurant's website URI"),
+                                    type=openapi.TYPE_STRING,
+                                    format="uri"
+                                ),
+                                'current_menu': openapi.Schema(
+                                    title=_("Menu"),
+                                    description=_("Currently published menu"),
+                                    type=openapi.TYPE_OBJECT,
+                                    properties={
+                                        'id': openapi.Schema(
+                                            description=_("Menu primary key"),
+                                            type=openapi.TYPE_INTEGER
+                                        ),
+                                    }
+                                )
+                            },
+                            required=['translations', 'stars', 'country', 'city', 'building', 'zip_code']
                         )
                     )
                 }
